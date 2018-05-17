eb8aea94fbc50678c6217faeee565af635dcf261
Sandeep Chugh <sandeep.chugh@live.com>
2018-05-16 22:02:37 -0500

    Added dbup project for mysql and docker compose for mysql

---
+++ src/Catalog.Databases.MySql/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+
+namespace Catalog.Databases.MySql
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            // TODO: Use Core 2.0 configuration api
+            var connectionString =
+                args.FirstOrDefault()??ConfigurationManager.ConnectionStrings["Products"].ConnectionString;
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ src/Catalog.Databases.MySql/Scripts/Script0001 - Create Tables.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE `catalog`.`Products` (
+  `ProductId` INT NOT NULL AUTO_INCREMENT,
+  `ProductUpc` VARCHAR(45) NOT NULL,
+  `ProductName` VARCHAR(45) NULL,
+  `ThumbnailImage` VARCHAR(200) NULL,
+  `LargeImage` VARCHAR(200) NULL,
+  `ListPrice` DECIMAL NULL,
+  `SalePrice` DECIMAL NULL,
+  `Description` TEXT NULL,
+  PRIMARY KEY (`ProductId`),
+  UNIQUE INDEX `ProductUpc_UNIQUE` (`ProductUpc` ASC));
